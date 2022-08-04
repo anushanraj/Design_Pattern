55f674e0ea1bad5a2c096ba52f3e6a19c7b5551a
anushanraj <107455695+anushanraj@users.noreply.github.com>
2022-08-04 22:09:31 +0530

    Day-3 Assignment1

---
+++ bookshow/BookMyMovieService.java
@@ -0,0 +1,5 @@
+package com.impact.bookshow;
+
+public class BookMyMovieService {
+
+}
---
+++ bookshow/Discount.java
@@ -0,0 +1,6 @@
+package com.impact.bookshow;
+
+public interface Discount {
+
+	double getDiscount(int noOfTickets);
+}
